a000795b6a74f68c8de086e3d3835be4786c2988
Fathy-elghoul <fh06802@gmail.com>
2024-09-06 16:41:38 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+// **** navbar ****
+const activeLinks = window.location.pathname
+const navLinks = [...document.querySelectorAll('nav .right-side ul li .hr')]
+
+navLinks.forEach(link =>{
+    if(link.href.includes(`${activeLinks}`)){
+        link.classList.add('active')
+    }
+})
+
+
+const sideList = document.querySelector('.side-lise')
+
+function actoin2(){
+    sideList.style.display = 'flex'
+}
+function exit(){
+    sideList.style.display = 'none'
+}
+
+let currentSlide = 1;
+
+
+
+
+
+
